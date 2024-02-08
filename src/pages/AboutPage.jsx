eec018a3c979a1e36e6f3f7cf198f01e7e3ca006
Teresa <tereac1102@gmail.com>
2024-02-08 17:49:59 +0100

    aboutpage style added

--- src/pages/AboutPage.jsx
+++ src/pages/AboutPage.jsx
@@ -5,17 +5,26 @@ function AboutPage() {
             <h2>Who are we?</h2>
             <p>We are a group of Ironhackers trying to learn how to develop Single Page Applications using React. This is a webpage designed to show available homes to rent for a day. Welcome to Eco Homes... AirBnB, but better!</p>
             <br />
+
             <h3>María Nogales</h3>
-            <a href="https://github.com/MariaNogales">GitHub</a>
-            <a href="https://www.linkedin.com/in/maria-nogales-8668111a5/">LinkedIn</a>
+            <ul>
+                <li> <a href="https://github.com/MariaNogales">GitHub</a></li>
+                <li><a href="https://www.linkedin.com/in/maria-nogales-8668111a5/">LinkedIn</a></li>
+            </ul>
             <br />
             <h3>Francisco Pazos</h3>
-            <a href="https://github.com/franpazos">GitHub</a>
-            <a href="https://www.linkedin.com/in/francisco-pazos-rey-bb37bb86/">LinkedIn</a>
+            <ul>
+                <li>  <a href="https://github.com/franpazos">GitHub</a></li>
+                <li> <a href="https://www.linkedin.com/in/francisco-pazos-rey-bb37bb86/">LinkedIn</a></li>
+            </ul>
             <br />
+
             <h3>Teresa Arranz</h3>
-            <a href="https://github.com/Tere1102">GitHub</a>
-            <a href="https://www.linkedin.com/in/teresa-arranz-carrasco-4336ba166/">LinkedIn</a>
+            <ul>
+                <li> <a href="https://github.com/Tere1102">GitHub</a></li>
+                <li>  <a href="https://www.linkedin.com/in/teresa-arranz-carrasco-4336ba166/">LinkedIn</a></li >
+
+            </ul>
 
         </div>
     )
